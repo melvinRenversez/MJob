db4f0d620e09c3384d671dc299b26056786727ee
melvinRenversez <120313822+melvinRenversez@users.noreply.github.com>
2024-10-06 15:13:57 +0200

    06/10/2024::15:13:57

---
+++ bash/export.bat
@@ -0,0 +1,3 @@
+cd c:\Program Files\MySQL\MySQL Server\bin
+
+mysqldump -u root -p mamanJob > C:\xampp\htdocs\MJob\db\mamanJob_backup.sql
---
+++ bash/import.bat
@@ -0,0 +1,3 @@
+cd c:\Program Files\MySQL\MySQL Server\bin
+
+mysql -u root -p mamanJob < C:\xampp\htdocs\MJob\db\mamanJob.sql
