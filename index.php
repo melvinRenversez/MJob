c76cfe2f26dc766b0f99baf5eec15889e1250836
melvinRenversez <163991271+louanMel@users.noreply.github.com>
2024-10-09 07:03:15 +0200

    from style

--- index.php
+++ index.php
@@ -3,19 +3,23 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
     <title>Connexion</title>
 </head>
 <body>
-    <h2>Connexion</h2>
-    <form action="php/connection.php" method="POST">
-        <label for="nom_utilisateur">Nom d'utilisateur :</label>
-        <input type="text" name="nom_utilisateur" required>
-        <br>
-        <label for="mot_de_passe">Mot de passe :</label>
-        <input type="password" name="mot_de_passe" required>
-        <br>
-        <input type="submit" value="Se connecter">
-    </form>
+    <div class="content">
+        <h1>Connexion</h1>
+        <form action="php/connection.php" method="POST">
+            <div class="inputLabel">
+                <label for="nom_utilisateur">Nom d'utilisateur</label>
+                <input type="text" name="nom_utilisateur" required>
+            </div>
+            <div class="inputLabel">
+                <label for="mot_de_passe">Mot de passe</label>
+                <input type="password" name="mot_de_passe" required>
+            </div>
+            <input type="submit" value="Se connecter">
+        </form>
+    </div>
 </body>
 </html>
-
