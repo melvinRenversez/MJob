30605eb217da617d4622f654bc8789cbbfe248cc
melvinRenversez <163991271+louanMel@users.noreply.github.com>
2024-10-10 14:14:02 +0200

    del

--- bash/export.bat
+++
@@ -1,3 +0,0 @@
-cd c:\Program Files\MySQL\MySQL Server\bin
-
-mysqldump -u root -p mamanJob > C:\xampp\htdocs\MJob\db\mamanJob_backup.sql
--- bash/import.bat
+++
@@ -1,3 +0,0 @@
-cd c:\Program Files\MySQL\MySQL Server\bin
-
-mysql -u root -p mamanJob < C:\xampp\htdocs\MJob\db\mamanJob.sql
