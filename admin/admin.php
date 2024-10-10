27e573e123e67b5be7f36bad6262bb32ebb4fd90
melvinRenversez <163991271+louanMel@users.noreply.github.com>
2024-10-10 14:26:17 +0200

    title

--- admin/admin.php
+++ admin/admin.php
@@ -14,7 +14,7 @@ include '../php/database.php';
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Admin</title>
     <link rel="stylesheet" href="style.css">
 </head>
 <body>
--- enfant/enfant.php
+++ enfant/enfant.php
@@ -21,7 +21,7 @@ if(isset($_GET['id'])) {
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="style.css">
-    <title>Document</title>
+    <title>Repas</title>
 </head>
 <body>
 
